ccd8bcd991c483713f7abd274f48fe50a3ed050f
irinasayshi <irinagorelik@Irinas-MacBook-Pro.local>
2018-03-24 21:49:24 -0600

    new js for header

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,19 @@
+$(document).ready(function(){
+
 // Fade out name and logo in image
 $(window).scroll(function(){
 	$(".child-name").css("opacity", 1 - $(window).scrollTop() / 250);
+});
+
+var offset = 100;
+var duration = 700;
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > offset) {
+$(".about-header").fadeIn(duration);
+} else {
+$(".about-header").fadeOut(duration);
+}
+});
+
 });
